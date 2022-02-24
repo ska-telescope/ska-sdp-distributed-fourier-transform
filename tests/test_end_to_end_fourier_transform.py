20f7cb18733ba9ad7bbf863c64d3ed8d876b40dc
Gabriella Hodosán <hodosan.gabriella@gmail.com>
2022-02-24 13:28:03 +0000

    ORC-1196 Docstring fix

--- tests/test_end_to_end_fourier_transform.py
+++ tests/test_end_to_end_fourier_transform.py
@@ -106,7 +106,7 @@ def test_end_to_end_1d_dask(use_dask, dask_option):
 @pytest.mark.parametrize("use_dask", [False, True])
 def test_end_to_end_2d_dask(use_dask):
     """
-    Test that the 1d algorithm produces the same results without dask.
+    Test that the 2d algorithm produces the same results with and without dask.
     """
     # Fixing seed of numpy random
     numpy.random.seed(123456789)
