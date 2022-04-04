43f356a0bdddb444fa9f642a7b1ae727da90db70
cnwangfeng <cnwangfeng@gmail.com>
2022-04-04 12:02:44 +0800

    ORC-1214: fixed scatter

--- src/fourier_transform/fourier_algorithm.py
+++ src/fourier_transform/fourier_algorithm.py
@@ -236,6 +236,7 @@ def make_subgrid_and_facet(G, FG, constants_class, dims, use_dask=False):
             (
                 constants_class.nsubgrid,
                 constants_class.nsubgrid,
+                # TODO: (WF) - Need to remove 2 lines if using Dask
                 constants_class.xA_size,
                 constants_class.xA_size,
             ),
