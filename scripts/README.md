0725b81d01a554514a95d388ba84c8d6cf934178
cnwangfeng <cnwangfeng@gmail.com>
2022-06-15 14:12:55 +0800

    ORC-1314: optimise loops

--- scripts/README.md
+++ scripts/README.md
@@ -9,7 +9,11 @@ using `class StreamingDistributedFFT`
 python scripts/memory_consumption.py --swift_config 8k[1]-n4k-512
 ```
 
-`performance_queue.py` can be used to evaluate the performance of Dask execution using queue 
+`performance_queue.py` can be used to evaluate the memory consumption and performance of Dask execution using queue 
 and batch mode optimization. The command is :
 ```bash
 python scripts/performance_queue.py --swift_config 8k[1]-n4k-512 --hdf5_prefix path/to/data
+```
+If you want to verify and output approximate values of subgrid in hdf5 format, you need to use the parameter `--check_results=True`
+
+Parameters `--max_work_tasks`, `--max_NMBF_BF_waiting_task` and `--batch_i1_number_task` can adjust the number of batches to improve execution efficiency
--- scripts/performance_queue.py
+++ scripts/performance_queue.py
@@ -1,13 +1,12 @@
 # pylint: disable=chained-comparison,too-many-arguments
 # pylint: disable=unused-argument,too-many-statements
-# pylint: disable=logging-fstring-interpolation
+# pylint: disable=logging-fstring-interpolation，too-many-branches
 """
 Testing Distributed Fourier Transform Testing for evaluating performance.
 """
 import itertools
 import logging
 import os
-import sys
 
 import dask
 import dask.array
@@ -17,7 +16,12 @@ import numpy
 from distributed import performance_report
 from distributed.diagnostics import MemorySampler
 
-from scripts.utils import batch_NMBF_NMBF_sum_finish_subgrid, wait_for_tasks
+from scripts.utils import (
+    batch_all_i1_NMBF_NMBF,
+    batch_NMBF_NMBF_sum_finish_subgrid,
+    wait_for_tasks,
+    write_approx_subgrid,
+)
 from src.fourier_transform.algorithm_parameters import (
     BaseArrays,
     StreamingDistributedFFT,
@@ -28,23 +32,140 @@ from src.fourier_transform.fourier_algorithm import (
 )
 from src.fourier_transform_dask import cli_parser
 from src.swift_configs import SWIFT_CONFIGS
-from src.utils import generate_input_data
+from src.utils import error_task_facet_to_subgrid_2d, generate_input_data
 
 log = logging.getLogger("fourier-logger")
 log.setLevel(logging.INFO)
-log.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def cli_parser_with_batch():
+    """
+    Add some arguments for batch
+    """
+    parser = cli_parser()
+
+    parser.add_argument(
+        "--max_work_tasks",
+        type=int,
+        default=9,
+        help="Maximum number of tasks in the queue",
+    )
+    parser.add_argument(
+        "--max_NMBF_BF_waiting_task",
+        type=int,
+        default=3,
+        help="NMBF_BF Number of waiting tasks",
+    )
+    parser.add_argument(
+        "--batch_i1_number_task",
+        type=int,
+        default=26,
+        help="How many i1's as a batch, eg: in 96k-n48k-512, "
+        "this number is 26",
+    )
+    parser.add_argument(
+        "--check_results",
+        type=str,
+        default="False",
+        help="if save and check results",
+    )
+    return parser
+
+
+@dask.delayed
+def check_batch_i1(
+    i0, i1_list, approx_subgrid_i1_list, true_subgrid_i1_list, base_arrays
+):
+    """
+    check a batch of approx_subgrid
+
+    :param i0: i0 index
+    :param i1_list: a batch of i1 index
+    :param approx_subgrid_i1_list: a batch of approx_subgrid
+    :param true_subgrid_i1_list: a batch of true_subgrid
+    :param base_arrays: BaseArrays class object
+
+    :return List of i0,i1 and error of approx_subgrid
+    """
+    i1_res = []
+    for i1 in i1_list:
+        err_res = error_task_facet_to_subgrid_2d(
+            approx_subgrid_i1_list[i1],
+            true_subgrid_i1_list[i1],
+            base_arrays.nsubgrid,
+            use_dask=False,
+            nout=1,
+        )
+        i1_res.append((i0, i1, numpy.average(err_res)))
+    return i1_res
+
+
+def check_approx_subgrid(
+    fundamental_params,
+    hdf5_prefix=None,
+    hdf5_chunksize_G=256,
+    hdf5_chunksize_FG=256,
+):
+    """
+    check all approx subgrid using dask
+
+    :param fundamental_params: dictionary of fundamental parmeters
+                               chosen from swift_configs.py
+    :param hdf5_prefix: hdf5 path prefix
+    :param hdf5_chunksize_G: hdf5 chunk size for G data
+    :param hdf5_chunksize_G: hdf5 chunk size for FG data
+    """
+    base_arrays = BaseArrays(**fundamental_params)
+    G_2_file = f"{hdf5_prefix}/G_{base_arrays.N}_{hdf5_chunksize_G}.h5"
+    FG_2_file = f"{hdf5_prefix}/FG_{base_arrays.N}_{hdf5_chunksize_FG}.h5"
+
+    approx_G_2_file = (
+        f"{hdf5_prefix}/approx_G_{base_arrays.N}_{hdf5_chunksize_G}.h5"
+    )
+
+    true_subgrid, _ = make_subgrid_and_facet_from_hdf5(
+        G_2_file,
+        FG_2_file,
+        base_arrays,
+        use_dask=True,
+    )
+
+    approx_subgrid, _ = make_subgrid_and_facet_from_hdf5(
+        approx_G_2_file,
+        FG_2_file,
+        base_arrays,
+        use_dask=True,
+    )
+    err_res_task = []
+    for i0 in range(base_arrays.nsubgrid):
+        while len(err_res_task) >= 3:
+            err_res_task = wait_for_tasks(err_res_task)
+        approx_subgrid_i1_list = []
+        true_subgrid_i1_list = []
+        for i1 in range(base_arrays.nsubgrid):
+            approx_subgrid_i1_list.append(approx_subgrid[i0][i1])
+            true_subgrid_i1_list.append(true_subgrid[i0][i1])
+        i1_res = check_batch_i1(
+            i0,
+            list(range(base_arrays.nsubgrid)),
+            approx_subgrid_i1_list,
+            true_subgrid_i1_list,
+            base_arrays,
+        )
+        err_res_task += dask.compute(i1_res, sync=False)
+    wait_for_tasks(err_res_task)
 
 
 def run_facet_to_subgrid_with_batch(
     fundamental_params,
-    to_plot=True,
-    fig_name=None,
-    use_dask=False,
-    client=None,
-    use_hdf5=False,
+    use_dask=True,
     hdf5_prefix=None,
     hdf5_chunksize_G=256,
     hdf5_chunksize_FG=256,
+    max_work_tasks=9,
+    max_NMBF_BF_waiting_task=3,
+    batch_i1_number_task=26,
+    check_results=False,
 ):
     """
     Main execution function that reads in the configuration,
@@ -56,15 +177,10 @@ def run_facet_to_subgrid_with_batch(
                      PNG files. If to_plot is set to False,
                      fig_name doesn't have an effect.
     :param use_dask: boolean; use Dask?
-    :param client: Dask client or None
-    :param use_hdf5: use Hdf5?
     :param hdf5_prefix: hdf5 path prefix
     :param hdf5_chunksize_G: hdf5 chunk size for G data
     :param hdf5_chunksize_G: hdf5 chunk size for FG data
-    :return: subgrid_2, facet_2, approx_subgrid, approx_facet
-                when use_hdf5=False
-             subgrid_2_file, facet_2_file, approx_subgrid_2_file,
-                approx_facet_2_file when use_hdf5=True
+    :return: dataframe of MemorySampler
     """
     base_arrays = BaseArrays(**fundamental_params)
     distr_fft = StreamingDistributedFFT(**fundamental_params)
@@ -93,6 +209,21 @@ def run_facet_to_subgrid_with_batch(
                 chunks=(hdf5_chunksize_FG, hdf5_chunksize_FG),
             )
             FG_dataset[:] = FG_2.astype("complex128")[:]
+
+    if check_results:
+        # create empty approx_G2 hdf5
+        approx_G_2_file = (
+            f"{hdf5_prefix}/approx_G_{base_arrays.N}_{hdf5_chunksize_G}.h5"
+        )
+        if not os.path.exists(approx_G_2_file):
+            with h5py.File(approx_G_2_file, "w") as f:
+                f.create_dataset(
+                    "G_data",
+                    (base_arrays.N, base_arrays.N),
+                    dtype="complex128",
+                    chunks=(hdf5_chunksize_G, hdf5_chunksize_G),
+                )
+
     _, facet_2 = make_subgrid_and_facet_from_hdf5(
         G_2_file,
         FG_2_file,
@@ -100,7 +231,7 @@ def run_facet_to_subgrid_with_batch(
         use_dask=use_dask,
     )
     # Calculate expected memory usage
-    max_work_tasks = 9
+
     cpx_size = numpy.dtype(complex).itemsize
 
     log.info(" == Expected memory usage:")
@@ -134,6 +265,7 @@ def run_facet_to_subgrid_with_batch(
     facet_ixs = list(
         itertools.product(range(distr_fft.nfacet), range(distr_fft.nfacet))
     )
+
     ms = MemorySampler()
     with ms.sample("NMBF_NMBF.process", measure="process"):
         with ms.sample("NMBF_NMBF.managed", measure="managed"):
@@ -155,11 +287,14 @@ def run_facet_to_subgrid_with_batch(
             # Job queue
             work_tasks = []
             # Sequentially go over subgrid columns
+
             all_i0_list = list(range(distr_fft.nsubgrid))
-            MAX_ppp_task = 3
             batch_i0_list = []
-            for i in range(0, len(all_i0_list), MAX_ppp_task):
-                batch_i0_list.append(all_i0_list[i : i + MAX_ppp_task])
+            for i in range(0, len(all_i0_list), max_NMBF_BF_waiting_task):
+                batch_i0_list.append(
+                    all_i0_list[i : i + max_NMBF_BF_waiting_task]
+                )
+
             for batch_i0 in batch_i0_list:
                 NMBF_BF_tasks_batch = []
                 for i0 in batch_i0:
@@ -185,35 +320,62 @@ def run_facet_to_subgrid_with_batch(
                         ]
                     )[0]
                     NMBF_BF_tasks_batch.append(NMBF_BF_tasks)
+
                 for idx_i0, i0 in enumerate(batch_i0):
                     # ** Step 3: Extraction second axis
                     # Sequential go over individual subgrids
+
+                    while len(work_tasks) >= max_work_tasks:
+                        work_tasks = wait_for_tasks(
+                            work_tasks, return_when="FIRST_COMPLETED"
+                        )
+
                     all_i1_list = list(range(distr_fft.nsubgrid))
-                    seq_i1_task = 10
                     batch_i1_list = []
-                    for i in range(0, len(all_i1_list), seq_i1_task):
-                        batch_i1_list.append(all_i1_list[i : i + seq_i1_task])
-                    for i1_batch in batch_i1_list:
-                        while len(work_tasks) >= max_work_tasks:
-                            work_tasks = wait_for_tasks(
-                                work_tasks, return_when="FIRST_COMPLETED"
-                            )
+                    for i in range(0, len(all_i1_list), batch_i1_number_task):
+                        batch_i1_list.append(
+                            all_i1_list[i : i + batch_i1_number_task]
+                        )
+
+                    # compute NMBF_NMBF with all i1 axis in one task
+                    batch_all_i1_NMBF_NMBF_list = []
+                    for (j0, j1), NMBF_BF in zip(
+                        facet_ixs, NMBF_BF_tasks_batch[idx_i0]
+                    ):
+                        batch_all_i1_NMBF_NMBF_list.append(
+                            dask.delayed(
+                                batch_all_i1_NMBF_NMBF, nout=len(batch_i1_list)
+                            )(NMBF_BF, distr_fft, base_arrays, batch_i1_list)
+                        )
+
+                    for idx_i1, i1_batch in enumerate(batch_i1_list):
+
+                        nfacet_ni1_batch_list = []
+                        for F_batch in batch_all_i1_NMBF_NMBF_list:
+                            nfacet_ni1_batch_list.append(F_batch[idx_i1])
+
                         batch_subgrid_task = (
                             batch_NMBF_NMBF_sum_finish_subgrid(
-                                NMBF_BF_tasks_batch[idx_i0],
+                                nfacet_ni1_batch_list,
                                 distr_fft,
                                 base_arrays,
                                 facet_ixs,
                                 i0,
                                 i1_batch,
+                                check=check_results,
                             )
                         )
+
+                        if check_results:
+                            batch_subgrid_task = write_approx_subgrid(
+                                batch_subgrid_task,
+                                base_arrays,
+                                approx_G_2_file,
+                            )
                         work_tasks += dask.compute(
                             batch_subgrid_task, sync=False
                         )
-                    wait_for_tasks(work_tasks)
-                    # clean batch
-                    NMBF_BF_tasks_batch[idx_i0] = None
+
                 # Finish tasks
                 wait_for_tasks(work_tasks)
     ms_df = ms.to_pandas()
@@ -246,20 +408,30 @@ def main(args):
     dask_client = set_up_dask(scheduler_address=scheduler)
     for config_key in swift_config_keys:
         log.info("Running for swift-config: %s", config_key)
+        # step: facet to approx_subgrid and write hdf5
         with performance_report(filename=f"dask-report-{config_key}.html"):
             ms_df = run_facet_to_subgrid_with_batch(
                 SWIFT_CONFIGS[config_key],
-                to_plot=False,
-                use_dask=True,
                 hdf5_prefix=args.hdf5_prefix,
-                client=dask_client,
+                max_work_tasks=args.max_work_tasks,
+                max_NMBF_BF_waiting_task=args.max_NMBF_BF_waiting_task,
+                batch_i1_number_task=args.batch_i1_number_task,
+                check_results=args.check_results == "True",
             )
             ms_df.to_csv(f"seq_more_seq_{config_key}.csv")
+
+        # step: check subgrid and approx_subgrid
+        if args.check_results == "True":
+            check_approx_subgrid(
+                SWIFT_CONFIGS[config_key],
+                hdf5_prefix=args.hdf5_prefix,
+            )
+
         dask_client.restart()
     tear_down_dask(dask_client)
 
 
 if __name__ == "__main__":
-    dfft_parser = cli_parser()
+    dfft_parser = cli_parser_with_batch()
     parsed_args = dfft_parser.parse_args()
     main(parsed_args)
