a5324eccb1d80bd314bbafb9d74b99ede3140349
luminosa42 <yl790@cam.ac.uk>
2022-08-30 09:54:15 +0100

    ORC-1369: Revert to last version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -22,8 +22,8 @@ import sys
 from unittest import mock
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../../src"))
-sys.path.insert(0, os.path.abspath("../../scripts"))
+#sys.path.insert(0, os.path.abspath("../../src"))
+#sys.path.insert(0, os.path.abspath("../../scripts"))
 
 
 def setup(app):
