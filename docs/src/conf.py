e1feced3d73347fc5dea8b8538ccc1194764e8a8
luminosa42 <yl790@cam.ac.uk>
2022-08-30 10:28:02 +0100

    ORC-1369: Clean up

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -24,7 +24,7 @@ from unittest import mock
 # Define build directories
 sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("../../src"))
-#sys.path.insert(0, os.path.abspath("../../scripts"))
+sys.path.insert(0, os.path.abspath("../../scripts"))
 
 
 def setup(app):
@@ -52,7 +52,6 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx_automodapi.automodapi",
     "sphinx_automodapi.smart_resolver",
-#    'sphinxarg.ext',
 ]
 
 autodoc_mock_imports = [
